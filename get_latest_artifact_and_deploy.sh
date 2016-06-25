830241f4579b05121e89074af764d7a56454342f
Eric <fcp.gris@gmail.com>
2016-06-25 23:01:56 +0800

    add deploy script

---
+++ get_latest_artifact_and_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -f git-user-spy*
+wget `echo -n "http://45.32.8.39:8081/artifactory/libs-snapshot-local/com/eirc/git-user-spy/0.0.1-SNAPSHOT/";curl --silent http://45.32.8.39:8081/artifactory/libs-snapshot-local/com/eirc/git-user-spy/0.0.1-SNAPSHOT/ | grep git-user-spy-0.0.1 | grep -v md5 | grep -v sha1 | tail -n 1 | sed -e 's|^<a.*">\(.*\)</a>.*|\1|'`
+mv git-user-spy*.jar git-user-spy.jar
+killall java
+java -jar git-user-spy.jar &
+sleep 10
