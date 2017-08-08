049466fabb86579a4d39d5b247d55d9b1de9d82e
lizhang <lizhang@freewheel.tv>
2017-08-08 22:52:23 +0800

    fix typo

--- deploy.pp
+++ deploy.pp
@@ -2,7 +2,7 @@ package { 'java-1.8.0-openjdk':
   ensure => present,
 }
 
-exec { 'nohup java -jar git_user_spy.jar &':
+exec { 'nohup java -jar git-user-spy.jar &':
   cwd     => '/home/ec2-user',
   path    => ['/usr/bin', '/usr/sbin',],
 }
