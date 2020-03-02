c247c9c1075382217f2d9099a9615cc94120fa26
Eric Zhang <eric.5.zhang@nokia-sbell.com>
2020-03-02 12:00:37 +0800

    remove old code

--- deploy.pp
+++
@@ -1,8 +0,0 @@
-package { 'java-1.8.0-openjdk':
-  ensure => present,
-}
-
-exec { 'nohup java -jar git-user-spy.jar &':
-  cwd     => '/home/ec2-user',
-  path    => ['/usr/bin', '/usr/sbin',],
-}
--- test_node.pp
+++
@@ -1,8 +0,0 @@
-package { 'openjdk-8-jdk':
-    ensure => present,
-}
-
-exec{'get_latest_artifact_and_deploy':
-  command => "/root/get_latest_artifact_and_deploy.sh &",
-}
-
